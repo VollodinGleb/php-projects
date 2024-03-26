78e6c18e5a44ab87df157e97007e84ec1eb81286
G L E B U S <glebvollodin@gmail.com>
2024-03-26 15:45:32 +0200

    add: got task for protecting 4, 5 labs

---
+++ pr4/task.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Захист 4 5 лаб</h2>
+    <p>Пошук факторіала числа введенного в інпут, не можеш ввести в інпут - та тип char, приймає цілі числа</p>
+</body>
+</html>
