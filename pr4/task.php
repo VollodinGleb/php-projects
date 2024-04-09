7764648ad8f3c9d55868ba10ecb5e924e781b8ae
G L E B U S <glebvollodin@gmail.com>
2024-04-09 14:52:27 +0300

    add: covered 4-5 prs

--- pr4/task.php
+++ pr4/task.php
@@ -1,12 +1,36 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $inputNumber = isset($_POST['number']) ? intval($_POST['number']) : 0;
+
+        if ($inputNumber >= 0 && is_int($inputNumber)) {
+            function factorial($n) {
+                if ($n === 0) {
+                    return 1;
+                } else {
+                    return $n * factorial($n - 1);
+                }
+            }
+            $result = factorial($inputNumber);
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Facrtorial</title>
 </head>
 <body>
     <h2>Захист 4 5 лаб</h2>
-    <p>Пошук факторіала числа введенного в інпут, не можеш ввести в інпут - та тип char, приймає цілі числа</p>
+
+    <form method="post">
+        <label for="number">Введіть число:</label>
+        <input type="number" id="number" name="number" required>
+        <button type="submit">Обчислити</button>
+        <br>
+        <label for="result">Результат:</label>
+        <textarea id="result" name="result" rows="4" cols="50" readonly><?php echo $result  ; ?></textarea>
+    </form>
 </body>
 </html>
